36af1c970d1d564368c9e273ef5f80b4f848448d
chim borey <oceansdtol@gmail.com>
2026-09-02 18:37:13 +0700

    Update

--- src/Modules/Security/Parents/Security.jsx
+++ src/Modules/Security/Parents/Security.jsx
@@ -1,9 +1,10 @@
+
 import Security_Header from "../Children/Security_header";
 import Security_Settings from "../Children/Security_Settings";
 
 
-export default  function Security(){
-      return(
+export default function Security() {
+      return (
             <section className=" w-full overflow-hidden space-y-7">
                   {/* ========================== Security_Header ========================== */}
                   <Security_Header />
